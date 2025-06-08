5dde06da696e89cbea7614fbac5c4fb4bedd1f6a
tevasansan <ryuteva.register@gmail.com>
2025-06-08 21:11:16 +0900

    バグ取り

--- track_test.py
+++ track_test.py
@@ -86,7 +86,11 @@ class SlaveUART:
                     self._parse_packet(p)
             #time.sleep(0.002)
 
-    def _parse_packet(self, data):
+    def _parse_write(self, params):
+        with self.mem_lock:
+            addr = int(params[0])
+            self.memory[addr:addr + len(params[1:])] = bytearray(params[1:])
+        self.send_packet(self.id, 0x00, None)
         if len(data) < 6:
             return  # 不完全パケット無視
         if data[0] != 0xFF or data[1] != 0xFF:
@@ -215,10 +219,10 @@ class Camera:
         x_c = self.fc_kinv @ pc
         d_w = self.fc_Rinv @ x_c  # ワールド系での視線ベクトル
 
-        s = -self.C[2] / d_w[2]            # Z=0との交点スケール
-        P = self.C + s * d_w               # ワールド座標上の交点
+        s = -self.fc_C[2] / d_w[2]            # Z=0との交点スケール
+        P = self.fc_C + s * d_w               # ワールド座標上の交点
+        delta = P - self.fc_C
 
-        delta = P - self.C
         angle = np.arctan2(delta[0], delta[1]) * 180 / np.pi  # XY平面上の角度（度）
         return angle, P
 
@@ -237,28 +241,31 @@ if __name__ == "__main__":
     try:
         while True:
             img = cam.get_front_camera()
-            if img is not None:
+            if img is None:
                 time.sleep(0.1)
                 print("cannot aquire camera image!")
                 continue
             outputs = objdet.predict(img)
+            print("outputs:", outputs)
             if outputs is not None:
-                
+                print("classes:", outputs[:, 6])
+                print("scores:", outputs[:, 4] * outputs[:, 5])
                 bboxes = outputs[:, 0:4]
                 cls = outputs[:, 6]
                 scores = outputs[:, 4] * outputs[:, 5]
-                redballs = bboxes[(cls == 1) & (scores > 0.6), 0:4]
+                redballs = bboxes[(cls == 0) & (scores > 0.6), 0:4]
 
                 areas = np.abs(redballs[:, 2] - redballs[:, 0]) * np.abs(redballs[:, 3] - redballs[:, 1])
                 #max_index = np.argmax(areas)
-                if redballs:
+                if redballs.size > 0:
                     target = redballs[0]
                     center = [(target[0]+target[2]) /2, (target[1]+target[3]) /2]
                     print(center)
-                    _, img_center = img.shape[:2]
+                    h, w = img.shape[:2]   
+                    img_center_x = w / 2
                     
-                    if abs(center[0] - img_center) > 25:
-                        if (center[0] - img_center) > 0:
+                    if abs(center[0] - img_center_x) > 25:
+                        if (center[0] - img_center_x) > 0:
                             print("turn right!!")
                             slave.set_data(0x02, True)
                             slave.set_data(0x03, 0.0)
@@ -273,14 +280,15 @@ if __name__ == "__main__":
                         slave.set_data(0x02, False)
                         slave.set_data(0x03, 0.0)
                         slave.set_data(0x07, 0.0)
+                    print(f"object 2dpos:  {center}, estimated 3dpos: {cam.fc_convert_2dpos_to_3d(center)}")
+
                 else:
                     print("there is no redball!")
             else:
                 print("no detection acquired")
             time.sleep(0.1)
 
-            print(f"object 2dpos:  {img_center}estimated 3dpos: {cam.fc_convert_2dpos_to_3d(img_center)}")
-
+            
     except KeyboardInterrupt:
         slave.serial_port.close()
         print("終了")
